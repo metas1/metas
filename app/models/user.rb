9b7d54d84df32f352c99ff1ad45f72af407fc63d
Karen Sanchez <karensanchez@Christophers-MacBook-Pro-3.local>
2017-02-01 13:56:28 -0500

    default pic

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-	has_attached_file :thumbnail, styles: { large: "300x300>", medium: "100x100>" }
+	has_attached_file :thumbnail, :default_url => 'profile-pic-250.png', styles: { large: "300x300>", medium: "100x100>" }
 	validates_attachment_content_type :thumbnail, content_type: /\Aimage\/.*\z/ 
-	
+	# has_attached_file :thumbnail, :default_url => 'profile-pic-250.png'
+
 	has_many :jobemails     
 end
