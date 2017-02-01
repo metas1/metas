fc1fbfaf1f03f1361c8f464a6c4809ca38d14930
Karen Sanchez <karensanchez@Christophers-MacBook-Pro-3.local>
2017-02-01 09:46:13 -0500

    range display

--- app/assets/javascripts/filters.js
+++ app/assets/javascripts/filters.js
@@ -1,19 +1,16 @@
 $(document).ready(function(){
 
-	$(".type-filter, .pay-range, .location-filter").change(function(){
+	$(".type-filter, .range-filter, .location-filter").change(function(){
 	    concatenated_string = $(".type-filter").val() + "+" + $(".range-filter").val() + "+" + $(".location-filter").val();
-	    console.log($(".type-filter").val() + " " + $(".location-filter").val())
 
 	    $("#submit-filters").val(concatenated_string);
-	    // console.log($("#submit-filters").val(concatenated_string));
-
-	    $("#temp_display").text(concatenated_string)
+	   		console.log(concatenated_string)	  
 	})
 
 	$("#clear-filters").click(function(){
 		$(this).closest("form").find("select").val("-1");
-		e.preventDefault();
-		console.log("filter test")
+		// e.preventDefault();
+		console.log("filters clear")
 	})
 
 });
